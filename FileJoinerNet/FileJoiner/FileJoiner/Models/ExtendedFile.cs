cb9aeb11f0d55235d9432694d8fa6e8a999340c9
Ihor Bodia <ihorbodia@gmail.com>
2020-10-29 23:50:25 +0100

    Drag and Drop

---
+++ FileJoinerNet/FileJoiner/FileJoiner/Models/ExtendedFile.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace FileJoiner.Models
+{
+    public class ExtendedFile
+    {
+        public ExtendedFile(string path)
+        {
+            File = new FileInfo(path);
+            Name = File.Name;
+            Type = File.Extension.Replace(".", "");
+            Size = File.Length / 1000;
+        }
+        public FileInfo File { get; private set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public long Size { get; set; }
+    }
+}
---
+++ FileJoinerNet/FileJoiner/FileJoiner/Views/MainDataGrid.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using FileJoiner.Models;
+using System.Collections.Generic;
+
+namespace FileJoiner.Views
+{
+    public class MainDataGrid : UserControl
+    {
+        DataGrid _DataGrid;
+        
+        public MainDataGrid()
+        {
+            this.InitializeComponent();
+
+            _DataGrid = this.FindControl<DataGrid>("mainDataGrid");
+            _DataGrid.IsReadOnly = true;
+
+            AddHandler(DragDrop.DropEvent, Drop);
+        }
+
+
+        private void Drop(object sender, DragEventArgs e)
+        {
+            var files = e.Data.GetFileNames();
+            var extendedFiles = new List<ExtendedFile>();
+            foreach (var file in files)
+            {
+                extendedFiles.Add(new ExtendedFile(file));
+            }
+            _DataGrid.Items = new DataGridCollectionView(extendedFiles);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
